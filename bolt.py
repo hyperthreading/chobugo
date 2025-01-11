d29ddc430c810e71f5707bde7e5150ec3be30249
parkchansoo <chansoo1563@gmail.com>
2025-01-11 22:52:15 +0900

    add ruff lint/format

--- bolt.py
+++ bolt.py
@@ -1,14 +1,21 @@
-
 import os
+
+from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# Initializes your app with your bot token and socket mode handler
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+from env import SLACK_BOT_TOKEN, SLACK_APP_TOKEN
 
-# Start your app
+load_dotenv()
+
+app = App(token=SLACK_BOT_TOKEN)
+
+@app.message("hello")
+def message_hello(message, say):
+    user = message["user"]
+    say(f"Hello, <@{user}>!")
+
+
+# # Start your app
 if __name__ == "__main__":
-    from dotenv import load_dotenv
-    load_dotenv()  # take environment variables from .env.
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-
+    SocketModeHandler(app, SLACK_APP_TOKEN).start()
---
+++ env.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SLACK_BOT_TOKEN=os.environ.get("SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN=os.environ.get("SLACK_APP_TOKEN")
