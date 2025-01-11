6f5f9e965d853674dd3e866dc114d60ff7bb2981
Yejun Kim <662691-hpthrd@users.noreply.replit.com>
2025-01-11 10:47:02 +0000

    init

---
+++ bolt.py
@@ -0,0 +1,14 @@
+
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Initializes your app with your bot token and socket mode handler
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# Start your app
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()  # take environment variables
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
