368e6de7b41b85f78f29d26c4919648f1bd70426
parkchansoo <chansoo1563@gmail.com>
2025-01-12 01:58:55 +0900

    revert to SocketModeHandler

--- bolt.py
+++ bolt.py
@@ -1,21 +1,19 @@
-
 from env import SLACK_BOT_TOKEN, SLACK_APP_TOKEN
-import os
-from slack_bolt.async_app import AsyncApp
-from slack_bolt.adapter.sanic import AsyncSlackRequestHandler
 
-app = AsyncApp(token=SLACK_BOT_TOKEN)
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Initializes your app with your bot token and socket mode handler
+app = App(token=SLACK_BOT_TOKEN)
 
 @app.event("app_mention")
 async def handle_app_mentions(say):
     await say("What's up?")
 
-handler = AsyncSlackRequestHandler(app)
-
 
 @app.message("hello")
 async def message_hello(message, say):
-    user = message["user"]
+    # user = message["user"]
     await say(blocks=[
         {
             "type": "section",
@@ -57,4 +55,4 @@ async def message_hello(message, say):
 
 # # Start your app
 if __name__ == "__main__":
-    app.start(3000)
+    SocketModeHandler(app, SLACK_APP_TOKEN).start()
