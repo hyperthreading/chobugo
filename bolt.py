b603ab7cf54672a25bf36f6b565d1abab7057d93
Jeng <blziiiin@kaist.ac.kr>
2025-01-12 02:46:56 +0900

    app.event not works

--- bolt.py
+++ bolt.py
@@ -7,24 +7,59 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 # Initializes your app with your bot token and socket mode handler
 app = App(token=SLACK_BOT_TOKEN)
 
-@app.event("app_mention")
-def handle_app_mentions(say):
-    say("What's up?")
+
+# @app.event("app_mention")
+# def handle_app_mentions(body, say):
+#     say("What's up?")
+
+
+@app.shortcut("open_modal")
+def open_modal(ack, body, client):
+    # Acknowledge the command request
+    ack()
+    # Call views_open with the built-in client
+    client.views_open(
+        # Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id=body["trigger_id"],
+        # View payload
+        view={
+            "type": "modal",
+            # View identifier
+            "callback_id": "view_1",
+            "title": {"type": "plain_text", "text": "My App"},
+            "submit": {"type": "plain_text", "text": "Submit"},
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {"type": "mrkdwn", "text": "Welcome to a modal with _blocks_"},
+                    "accessory": {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "Click me!"},
+                        "action_id": "button_abc"
+                    }
+                },
+                {
+                    "type": "input",
+                    "block_id": "input_c",
+                    "label": {"type": "plain_text", "text": "What are your hopes and dreams?"},
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "dreamy_input",
+                        "multiline": True
+                    }
+                }
+            ]
+        }
+    )
 
 
 @app.message("hello")
 def message_hello(message, say):
-    # user = message["user"]
+    global message_timestamp
     say(blocks=[
         {
             "type": "section",
-            "fields": [
-                {
-                    "type": "plain_text",
-                    "text": "오늘 수업 내용, 어떠셨나요?",
-                    "emoji": True
-                }
-            ]
+            "text": {"type": "plain_text", "text": "오늘 수업 내용, 어떠셨나요?"},
         },
         {
             "type": "actions",
@@ -34,24 +69,32 @@ def message_hello(message, say):
                     "text": {
                         "type": "plain_text",
                         "text": "재밌었어!",
-                        "emoji": True
                     },
-                    "value": "reponse_positive"
+                    "action_id": "reponse_positive"
                 },
                 {
                     "type": "button",
                     "text": {
                         "type": "plain_text",
                         "text": "글쎄..좀 어렵던데",
-                        "emoji": True
                     },
-                    "value": "response_negative"
+                    "action_id": "response_negative"
                 }
             ]
         }
-    ],
-        text="오늘 수업 내용, 어떠셨나요?"
-    )
+    ], text="오늘 수업 내용, 어떠셨나요?")
+
+    message_timestamp = message['ts']
+
+
+@app.action("reponse_positive")
+def handle_button_click(ack, body, say):
+    global message_timestamp
+    ack()
+    response_timestamp = body['actions'][0]['action_ts']
+    time_difference = float(
+        response_timestamp) - float(message_timestamp)
+    say(f"응답하는데 {int(time_difference)}초 걸렸습니다!")  # 버튼 클릭 후 메시지를 보냄
 
 
 # # Start your app
